bf69dbab0d8087b457b972dabf2e163a9192f8e4
Juan1511 <j15020611zulu@gmail.com>
2024-06-20 11:34:19 -0500

    se inicio el codigo del menu principal

--- proyecto.dart
+++ proyecto.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
 void main(List<String> args) {
+
+  // Definicion de variables
+  int opcion, gestion;
+  String? exposicionAgg;
+
+  // Imprimir menu
+
   print('BIENVENIDO AL MENU');
   print('Menú principal:');
   print('1. Gestión de temas');
@@ -7,4 +15,21 @@ void main(List<String> args) {
   print('4. Precargar datos de prueba');
   print('0. Salir');
   print('Ingrese la opción deseada: ');
+
+  // Opcion usuario
+  opcion = int.parse(stdin.readLineSync()!);
+
+  switch (opcion){
+    case 1:
+    print("--------------Gestion de temas:-------------- ");
+    print("1. Agregar exposicion");
+    print("2. Editar exposicion");
+    print("3. Borrar exposicion");
+    gestion = int.parse(stdin.readLineSync()!);
+    if(opcion == 1){
+      print("Cual es el nombre de la exposicion");
+      exposicionAgg = stdin.readLineSync();
+    }
+    break;
+  }
 }

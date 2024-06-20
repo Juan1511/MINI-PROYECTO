b6b34ef91478da10a0c8a30e806e8e2d91a92368
Juan1511 <j15020611zulu@gmail.com>
2024-06-20 10:42:35 -0500

    new file

---
+++ proyecto.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print('BIENVENIDO AL MENU');
+  print('Menú principal:');
+  print('1. Gestión de temas');
+  print('2. Gestión de estudiantes');
+  print('3. Asignar estudiantes aleatoriamente');
+  print('4. Precargar datos de prueba');
+  print('0. Salir');
+  print('Ingrese la opción deseada: ');
+}
